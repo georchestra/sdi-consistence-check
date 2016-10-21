15f15eb444b45a3507f2a903821503d1281eff11
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2016-10-21 16:22:14 +0200

    test - modifying CSW rquest
    so that the full iso MD is returned which allows owslib to correctly
    parse the OperatesOn fields (poke @Vampouille).

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 from getpass import getpass
 
+from owslib.csw import CatalogueServiceWeb, namespaces
+from owslib.fes import PropertyIsEqualTo, And
+
 from credentials import Credentials
 from cswquerier import CSWQuerier
 from inconsistency import LayerNotFoundInconsistency
 from owscheck import CachedOwsServices
 
-my_username = "xxxx"
-my_password = getpass()
-gnurl = 'https://sdi.georchestra.org/geonetwork/srv/eng/csw'
+my_username = None
+my_password = None
+gnurl = "http://www.geopicardie.fr/geonetwork/srv/fre/csw"
 
 csw_q = CSWQuerier(gnurl, username=my_username, password=my_password)
 
@@ -16,37 +19,53 @@ creds = Credentials()
 creds.addFromUrl(gnurl, my_username, my_password)
 geoserver_services = CachedOwsServices(creds)
 
-errors = []
+csw = CatalogueServiceWeb(gnurl, username=my_username, password=my_password)
+filter = [PropertyIsEqualTo("Type", "service")]
+csw.getrecords2(constraints=filter,
+                esn='full',
+                outputschema=namespaces['gmd'],
+                startposition=0,
+                maxrecords=1000000)
 
-while True:
-    print("---------------------------------------------------------------------------------------------")
-
-    res = csw_q.get_records()
-
-    # no more results, we should stop
-    if csw_q.csw.results['returned'] == 0:
-        break
-
-    for uuid in res:
-        print("\nUUID : %s" % uuid)
-
-#        for uri in csw.records[uuid].uris:
-        for uri in csw_q.get_md(uuid).uris:
-            # print("%s %s %s" % (uri['protocol'], uri['url'], uri['name']))
-            try:
-                if uri["protocol"] == "OGC:WMS":
-                    geoserver_services.checkWmsLayer(uri["url"], uri["name"])
-                    print("\tURI OK : %s %s %s" % (uri["protocol"], uri['url'], uri['name']))
-                elif uri["protocol"] == "OGC:WFS":
-                    geoserver_services.checkWfsLayer(uri["url"], uri["name"])
-                    print("\tURI OK : %s %s %s" % (uri["protocol"], uri['url'], uri['name']))
-                else:
-                    print("\tSkipping URI : %s %s %s" % (uri["protocol"], uri['url'], uri['name']))
-            except LayerNotFoundInconsistency as ex:
-                ex.set_md_uuid(uuid)
-                errors.append(ex)
-                print("\t /!\\ ---> Cannot find Layer ON GS : %s %s %s %s %s" % (uuid, uri['protocol'], uri['url'], uri['name'], ex))
+for uuid in csw.records:
+    if uuid != "0e1785b285ccecc853c52bdb4354b6a00df84cbe":
+        continue
+    md = csw.records[uuid]
+    for operateson in md.serviceidentification.operateson:
+        print ("Operates on %s" % operateson["href"])
 
 
-for error in errors:
-    print(error)
+# errors = []
+#
+# while True:
+#     print("---------------------------------------------------------------------------------------------")
+#
+#     res = csw_q.get_records()
+#
+#     # no more results, we should stop
+#     if csw_q.csw.results['returned'] == 0:
+#         break
+#
+#     for uuid in res:
+#         print("\nUUID : %s" % uuid)
+#
+# #        for uri in csw.records[uuid].uris:
+#         for uri in csw_q.get_md(uuid).uris:
+#             # print("%s %s %s" % (uri['protocol'], uri['url'], uri['name']))
+#             try:
+#                 if uri["protocol"] == "OGC:WMS":
+#                     geoserver_services.checkWmsLayer(uri["url"], uri["name"])
+#                     print("\tURI OK : %s %s %s" % (uri["protocol"], uri['url'], uri['name']))
+#                 elif uri["protocol"] == "OGC:WFS":
+#                     geoserver_services.checkWfsLayer(uri["url"], uri["name"])
+#                     print("\tURI OK : %s %s %s" % (uri["protocol"], uri['url'], uri['name']))
+#                 else:
+#                     print("\tSkipping URI : %s %s %s" % (uri["protocol"], uri['url'], uri['name']))
+#             except LayerNotFoundInconsistency as ex:
+#                 ex.set_md_uuid(uuid)
+#                 errors.append(ex)
+#                 print("\t /!\\ ---> Cannot find Layer ON GS : %s %s %s %s %s" % (uuid, uri['protocol'], uri['url'], uri['name'], ex))
+#
+#
+# for error in errors:
+#     print(error)
